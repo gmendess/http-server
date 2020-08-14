4afe84dcaff33382c478ff81a32ec2d81bffed6e
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-08-13 21:07:17 -0300

    atualizando

--- test/build.sh
+++ test/build.sh
@@ -5,5 +5,6 @@ mkdir -p ../obj/
 gcc -c ../src/server/server.c -o ../obj/server.o
 gcc -c ../src/util/util.c -o ../obj/util.o
 gcc -c ../src/http/request/request.c -o ../obj/request.o
+gcc -c ../src/http/methods/methods.c -o ../obj/methods.o
 
 gcc main.c ../obj/*.o -o main
