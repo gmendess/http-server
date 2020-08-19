09f9427b51167249793c539e5fa8c2d0fcc1b8d7
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-08-19 20:53:29 -0300

    atualizando build.sh

--- test/build.sh
+++ test/build.sh
@@ -2,11 +2,12 @@
 
 mkdir -p ../obj/
 
-gcc -c ../src/server/server.c -o ../obj/server.o
-gcc -c ../src/util/util.c -o ../obj/util.o
-gcc -c ../src/http/request/request.c -o ../obj/request.o
-gcc -c ../src/http/request/methods/methods.c -o ../obj/methods.o
-gcc -c ../src/http/response/status-codes/status_codes.c -o ../obj/status_codes.o
-gcc -c ../src/http/header/header.c -o ../obj/header.o
+gcc -c -Wall ../src/server/server.c -o ../obj/server.o
+gcc -c -Wall ../src/util/util.c -o ../obj/util.o
+gcc -c -Wall ../src/http/request/request.c -o ../obj/request.o
+gcc -c -Wall ../src/http/request/methods/methods.c -o ../obj/methods.o
+gcc -c -Wall ../src/http/response/status-codes/status_codes.c -o ../obj/status_codes.o
+gcc -c -Wall ../src/http/response/response.c -o ../obj/response.o
+gcc -c -Wall ../src/http/header/header.c -o ../obj/header.o
 
 gcc main.c ../obj/*.o -o main
