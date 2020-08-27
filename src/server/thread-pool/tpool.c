0daee374d7466f406e044702055d168bb9c7ad12
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-08-27 20:00:53 -0300

    Criada estrutura conn_queue_t, que representa uma fila de conexões
    Essa fila será usada pela rotina executada por cada thread da pool. Essa rotina consumirá as conexões retornadas da fila para gerenciá-las

--- src/server/thread-pool/tpool.c
+++ src/server/thread-pool/tpool.c
@@ -2,6 +2,7 @@
 #include <pthread.h>
 #include <stdio.h>
 #include "tpool.h"
+#include "../../util/util.h"
 
 int thread_pool_create(thread_pool_t* tpool) {
   int err = 0; // controle de erros
@@ -32,4 +33,68 @@ int thread_pool_create(thread_pool_t* tpool) {
 
   // retorna número de erros
   return total_errors;
+}
+
+int conn_queue_init(conn_queue_t* queue, size_t capacity) {
+  if(capacity == 0)
+    return -1;
+
+  int err = 0; // controle de erros
+
+  err |= pthread_mutex_init(&queue->mu, NULL);  
+  err |= sem_init(&queue->semaphore, 0, 0);
+
+  if(err) {
+    perror("conn_queue_init");
+    exit(EXIT_FAILURE); // erro fatal, não foi possível inicializar ou a mutex ou o semáforo
+  }
+
+  queue->connections = must_calloc(capacity, sizeof(int));
+ 
+  queue->capacity = capacity;
+  queue->head     = 0;
+  queue->tail     = 0;
+  queue->counter  = 0;
+
+  return 0;
+}
+
+int conn_enqueue(conn_queue_t* queue, int conn_fd) {
+  // adquire a mutex para fazer alterações na fila
+  pthread_mutex_lock(&queue->mu);
+  
+  // fila cheia
+  if(queue->counter == queue->capacity) {
+    pthread_mutex_unlock(&queue->mu);
+    return -1;
+  }
+
+  queue->connections[queue->tail] = conn_fd; // insere a conexão no vetor de conexões
+  queue->tail = (queue->tail + 1) % queue->capacity; // cria efeito de circularidade na fila
+  queue->counter++; // incrementa a quantidade de conexões na fila
+
+  // libera a mutex
+  pthread_mutex_unlock(&queue->mu);
+
+  // incrementa contador do semáforo, indicando que uma conexão foi adicionada na fila
+  sem_post(&queue->semaphore);
+  return 0;
+}
+
+int conn_dequeue(conn_queue_t* queue) {
+  // verifica se há alguma conexão a ser gerenciada, se não, espera por uma
+  sem_wait(&queue->semaphore);
+
+  // adquire mutex para fazer alterações e leitura na fila
+  pthread_mutex_lock(&queue->mu);
+
+  int ret_val = queue->connections[queue->head]; // adquire a conexão no inicio da fila
+  queue->head = (queue->head + 1) % queue->capacity; // move o inicio da fila
+  queue->counter--; // decrementa a quantidade de conexões
+
+  // libera a mutex
+  pthread_mutex_unlock(&queue->mu);
+
+  // retorna o file descriptor da conexão
+  return ret_val;
 }
--- src/server/thread-pool/tpool.h
+++ src/server/thread-pool/tpool.h
@@ -1,14 +1,68 @@
 #ifndef __THREAD_POOL_H
 #define __THREAD_POOL_H
 #include <pthread.h>
+#include <semaphore.h>
 
 #define THREAD_POOL_MAX_SIZE 16
 
+/*
+  Estrutura que representa uma pool de threads. Cada thread dessa pool ficará aguardando até
+  que uma conexão esteja disponível para ser gerenciada.
+*/
 typedef struct {
-  pthread_t threads[THREAD_POOL_MAX_SIZE];
-  size_t counter;
+  pthread_t threads[THREAD_POOL_MAX_SIZE]; // identificador de cada thread
+  size_t    counter; // quantidade de threads ativas
 } thread_pool_t;
 
+/*
+  Fila de conexões disponíveis para serem gerenciadas por uma das threads da pool.
+
+  Esse fila é implementada através de um vetor de inteiros e para isso foi implementado um algoritmo
+  para simular uma circularidade, isso fica mais claro nas funções enqueue e dequeue
+*/
+typedef struct {
+  pthread_mutex_t mu; // mutex para evitar race condition nos membros dessa struct
+  sem_t  semaphore;   // semáforo para indicar quando há conexões a serem retiradas da fila
+  size_t head;        // índice que corresponde ao inicio da fila (não é necessariamente 0)
+  size_t tail;        // índice que corresponde ao final da fila, ou seja, onde o próximo elemento será adicionado
+  size_t counter;     // número atual de elementos
+  size_t capacity;    // capacidade máxima da fila
+  int*   connections; // vetor que cada conexão(file descriptor) será enfileirada
+} conn_queue_t;
+
+/*
+  Inicia uma struct thread_pool_t com seus valores padrões e cria a quantidade de threads
+  especificadas por THREAD_POOL_MAX_SIZE
+
+  param tpool: ponteiro para struct thread_pool_t que será inicializada
+*/
 int thread_pool_create(thread_pool_t* tpool);
 
-#endif __THREAD_POOL_H
+/*
+  Inicializa os membros de uma struct conn_queue_t
+
+  @param queue: fila à ser inicializada
+  @param capacity: capacidade máxima de conexões que podem ser enfileiradas
+*/
+int conn_queue_init(conn_queue_t* queue, size_t capacity);
+
+/*
+  Enfileira uma conexão para ser consumida por uma das threads da pool
+*/
+int conn_enqueue(conn_queue_t* queue, int conn_fd);
+
+/*
+  Retira uma conexão da fila (usado pela threads da pool)
+
+  @param queue: fila em que a conexão será retirada
+*/
+int conn_dequeue(conn_queue_t* queue);
+
+/*
+  Destrói uma conn_queue_t, liberando seus recursos
+
+  @param queue: fila a ser destruída
+*/
+int conn_queue_destroy(conn_queue_t* queue);
+
+#endif // __THREAD_POOL_H
