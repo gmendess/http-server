058e98a769720749b58d2bcc4d1cea2c0de8f2e4
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-08-27 22:53:30 -0300

    atualizando build.sh

--- test/build.sh
+++ test/build.sh
@@ -3,6 +3,7 @@
 mkdir -p ../obj/
 
 gcc -c -Wall ../src/server/server.c -o ../obj/server.o
+gcc -c -Wall ../src/server/thread-pool/tpool.c -o ../obj/tpool.o
 gcc -c -Wall ../src/util/util.c -o ../obj/util.o
 gcc -c -Wall ../src/errors/errors.c -o ../obj/errors.o
 gcc -c -Wall ../src/http/request/request.c -o ../obj/request.o
@@ -11,4 +12,4 @@ gcc -c -Wall ../src/http/response/status-codes/status_codes.c -o ../obj/status_c
 gcc -c -Wall ../src/http/response/response.c -o ../obj/response.o
 gcc -c -Wall ../src/http/header/header.c -o ../obj/header.o
 
-gcc main.c ../obj/*.o -o main
+gcc main.c ../obj/*.o -lpthread -o main
