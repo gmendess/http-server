0567b8239768f630b4148e4374fb427958e8b2d7
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-08-28 20:31:56 -0300

    criado enum server_type_t, para identificar se é um threaded server ou não; adicionado membro conn_queue_t* em server_t; função config_threaded_server para configurar o server para usar threads e instânciar uma fila em que as conexões aceitas serão inseridas

--- src/server/server.h
+++ src/server/server.h
@@ -5,6 +5,7 @@
 #include "../http/response/response.h"
 #include "../http/request/methods/methods.h"
 #include "../errors/errors.h"
+#include "thread-pool/tpool.h"
 
 /*
   Fila de espera do socket; usado na syscall 'listen'
@@ -21,11 +22,20 @@ typedef struct __route_t {
   struct __route_t* next;    // próxima rota (NULL = fim da lista)
 } route_t;
 
+typedef enum {
+  FORKED,  // cada conexão é igual a um processo
+  #define FORKED   FORKED
+  THREADED // cada conexão é igual a uma thread
+  #define THREADED THREADED
+} server_type_t;
+
 typedef struct {
-  int listener;   // file descriptor do servidor (não usar no processo filho)
+  server_type_t type;  // threaded server ou forked server (a base de threads ou processos)
+  int listener;        // file descriptor do servidor (não usar no processo filho)
   char addr[INET6_ADDRSTRLEN]; // endereço do servidor
-  int  port;      // porta do servidor
-  route_t* route; // lista encadeada das rotas do servidor
+  int  port;           // porta do servidor
+  route_t* route;      // lista encadeada das rotas do servidor
+  conn_queue_t* queue; // fila de conexões (apenas p/ threaded-server, caso contrário será NULL)
 } server_t;
 
 /*
@@ -37,6 +47,16 @@ typedef struct {
   @para port: serviço (porta)
 */
 int new_http_server(server_t* server, const char* addr, const char* port);
+
+/*
+  Configura o servidor para ser um threaded server, ou seja, ele usará uma thread para lidar com cada
+  conexão. Essas threads fazem parte de uma thread pool.
+
+  @param server: servidor a ser configurado
+  @param queue_size: tamanho da fila de conexões; cada conexão aceita será enfileirada e posteriormente
+   pega por uma thread que estiver disponível na pool. 
+*/
+void config_threaded_server(server_t* server, size_t queue_size);
 
 /*
   Inicia o processo de escuta por requisição, programa entra em loop infinito esperando
