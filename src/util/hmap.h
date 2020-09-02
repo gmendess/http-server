1b964d6a0d08ab86f9a23f02d102cf8ab08fb1c8
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-09-01 21:54:24 -0300

    membro entries de hmap_t agora é um vetor de ponteiros para hmap_entry_t (fiz isso para economizar um pouco de memória); membro capacity de hmap_t renomeado para buckets

--- src/util/hmap.h
+++ src/util/hmap.h
@@ -3,28 +3,29 @@
 
 #include <stdlib.h>
 
-#define HMAP_DEFAULT_CAPACITY 256
+#define HMAP_DEFAULT_BUCKETS 256
 #define HMAP_NOT_FOUND -1
 #define HMAP_FULL      -2
 
 typedef unsigned long ussize_t;
 
-typedef struct {
+typedef struct hmap_entry {
   char* key;
   void* value;
   int   occupied;
+  struct hmap_entry* next;
 } hmap_entry_t;
 
 typedef struct {
-  size_t capacity;
+  size_t buckets;
   size_t counter;
-  hmap_entry_t* entries;
+  hmap_entry_t** entries;
 } hmap_t;
 
-void hmap_init(hmap_t* hmap, size_t capacity);
+void hmap_init(hmap_t* hmap, size_t buckets);
 void hmap_destroy(hmap_t* hmap);
 
-int hmap_add(hmap_t* hmap, char* key, void* value);
+int hmap_set(hmap_t* hmap, char* key, void* value);
 int hmap_get(hmap_t* hmap, char* key, void** output);
 
 #endif // __HASH_MAP_H
