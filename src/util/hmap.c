6d59879b38f4a51e7d57259bb93a252063cbbd34
Gabriel Mendes <gabriel.mendespng@gmail.com>
2020-09-01 21:55:53 -0300

    hmap_add renomeado para hmap_set; alterações nas funções hmap_set e hmap_get para lidarem com possíveis colisões; função estática free_entry_list para liberar a memória de todas as entries que estiverem ligadas pelo mesmo bucket, ou seja, que possuirem o mesmo index

--- src/util/hmap.c
+++ src/util/hmap.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "hmap.h"
 #include "util.h"
 
@@ -16,31 +17,64 @@ static ussize_t djb2_hash(char *key) {
   return hash;
 }
 
-void hmap_init(hmap_t* hmap, size_t capacity) {
-  if(capacity == 0)
-    capacity = HMAP_DEFAULT_CAPACITY;
+void hmap_init(hmap_t* hmap, size_t buckets) {
+  if(buckets == 0)
+    buckets = HMAP_DEFAULT_BUCKETS;
 
-  hmap->capacity = capacity;
-  hmap->counter  = 0;
-  hmap->entries  = must_calloc(capacity, sizeof(hmap_entry_t));
+  hmap->buckets = buckets;
+  hmap->counter = 0;
+  hmap->entries = must_calloc(buckets, sizeof(hmap_entry_t*));
+}
+
+static void free_entry_list(hmap_entry_t* entry) {
+  hmap_entry_t* prev = NULL;
+  while(entry) {
+    prev  = entry;
+    entry = entry->next;
+    free(prev);
+  }
 }
 
 void hmap_destroy(hmap_t* hmap) {
-  hmap->capacity = 0;
-  hmap->counter  = 0;
+  for(int i = 0; i < hmap->buckets; i++)
+    free_entry_list(hmap->entries[i]);
   free(hmap->entries);
+  hmap->buckets  = 0;
+  hmap->counter  = 0;
   hmap->entries  = NULL;
 }
 
-int hmap_add(hmap_t* hmap, char* key, void* value) {
-  ussize_t index = djb2_hash(key) % hmap->capacity;
+int hmap_set(hmap_t* hmap, char* key, void* value) {
+  ussize_t index = djb2_hash(key) % hmap->buckets;
 
-  if(hmap->counter == hmap->capacity)
-    return -1;
+  hmap_entry_t** entry = &hmap->entries[index];
 
-  hmap->entries[index].key      = key;
-  hmap->entries[index].value    = value;
-  hmap->entries[index].occupied = 1;
+  // bucket vazio, aloco memória para ele
+  if(*entry == NULL)
+    *entry  = must_calloc(1, sizeof(hmap_entry_t));
+  else if( (*entry)->occupied ) {
+    do {
+      // se a chave de entry for igual a passada via parâmetro será
+      // necessário alterar o valor de uma chave que já existe, encerro
+      // o loop sem precisar alocar memória
+      if(strcmp( (*entry)->key, key) == 0)
+        goto set;
+
+      // vá para a próxima entry da lista
+      entry = &(*entry)->next;
+    }while(*entry != NULL);
+
+    // cheguei ao final da lista, aloco memória para a nova entry que 
+    // será configurada logo abaixo
+    *entry = must_calloc(1, sizeof(hmap_entry_t));
+  }
+
+  // configurando os valores da nova entry
+set:
+  (*entry)->key      = key;
+  (*entry)->value    = value;
+  (*entry)->occupied = 1;
+
   hmap->counter++;
 
   return 0;
@@ -50,11 +84,24 @@ int hmap_get(hmap_t* hmap, char* key, void** output) {
   if(hmap->counter == 0)
     return HMAP_NOT_FOUND;
 
-  ussize_t index = djb2_hash(key) % hmap->capacity;
-  if(!hmap->entries[index].occupied)
+  ussize_t index = djb2_hash(key) % hmap->buckets;
+  hmap_entry_t* entry = hmap->entries[index];
+
+  // bucket ainda não alocado ou não ocupado
+  if(!entry || !entry->occupied)
     return HMAP_NOT_FOUND;
 
-  *output = hmap->entries[index].value;
+  // procuro a entry que tenha a chave solicitada 
+  while(strcmp(entry->key, key) != 0) {
+    entry = entry->next;
+
+    // se entry for NULL, cheguei ao final da lista sem encontrar
+    // a entry que tenha a chave desejada
+    if(entry == NULL)
+      return HMAP_NOT_FOUND;
+  }
+
+  *output = entry->value;
 
   return 0;
 }
